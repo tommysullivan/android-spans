a6af8069d094e04499d4a0c00c4c2c2756dec7d7
Tommy Sullivan <tommy@classdojo.com>
2018-11-20 01:59:22 +0000

    small changes

--- app/src/main/java/com/classdojo/android/spans/interfaces/NodeBuilderBasic.kt
+++ app/src/main/java/com/classdojo/android/spans/interfaces/NodeBuilderBasic.kt
@@ -4,5 +4,7 @@ interface NodeBuilderBasic<T : NodeBuilderBasic<T>> {
     //TODO: Implement addTranslatedText(@StringRes Integer translationId, NodeBuilder[] replacements);
     //TODO: Implement Builder based convenience functions so callers need not call build() first (even though reader will suffice)
     fun addStyledSpan(styleReader: StyleReader, spanBuilder: NodeReaderBasic): T
+
+    //TODO: Make build() return NodeReaderBasic or something that extends it
     fun build(): NodeReaderBasic
 }
